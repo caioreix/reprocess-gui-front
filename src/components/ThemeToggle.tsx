2baf20b35f091895ec4d36f37ff9ac41316608cd
Caio Almeida <caioreix@outlook.com>
2024-03-04 03:32:04 -0300

    Improve theme choose feature

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,13 +5,17 @@ import React, { PropsWithChildren, useState } from 'react';
 import { Classic } from '@theme-toggles/react';
 
 export const ThemeToggle: React.FC<PropsWithChildren> = ({ children }) => {
+    const isDark = (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
+    const [isToggled, setToggle] = useState(isDark)
 
-    const [isToggled, setToggle] = useState(false)
+    const onToggle = () => {
+        localStorage.theme = isToggled ? 'light' : 'dark'
+    }
 
     return (
-        <div className={isToggled ? '' : 'dark'}>
+        <div className={isToggled ? 'dark' : 'light'}>
             <div className=' fixed top-1.5 right-6 '>
-                <Classic placeholder="" toggled={isToggled} toggle={setToggle} className='text-zinc-700 dark:text-zinc-200 text-3xl' />
+                <Classic placeholder="" onToggle={onToggle} toggled={isToggled} toggle={setToggle} className='text-zinc-700 dark:text-zinc-200 text-3xl' />
             </div>
             {children}
         </div>
